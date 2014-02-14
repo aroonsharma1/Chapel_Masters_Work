964afaeb2644a8d5474f95535cd501a514c0cfba
Aroon Sharma <aroonsharma1@yahoo.com>
2014-02-14 15:49:19 -0500

    Bug with stream script should be fixed

--- Josh's Benchmarks/Benchmarks/HPCC/dostreambench.bash
+++ Josh's Benchmarks/Benchmarks/HPCC/dostreambench.bash
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 nl=10
-numVectors=10
 
 echo nl=$nl
-echo numVectors=10
 
 #compile stream
 chpl --fast stream.chpl -o stream
 
 echo 'Block (B)'
-./stream -nl $nl --dist=B --numVectors=$numVectors  --messages
-./stream -nl $nl --dist=B --numVectors=$numVectors  --timeit
+./stream -nl $nl --dist=B --messages
+./stream -nl $nl --dist=B --timeit
 
 echo 'Cyclic (C)'
-./stream -nl $nl --dist=C --numVectors=$numVectors  --messages
-./stream -nl $nl --dist=C --numVectors=$numVectors  --timeit
+./stream -nl $nl --dist=C --messages
+./stream -nl $nl --dist=C --timeit
 
 echo 'Cyclic with modulo unrolling (CM)'
-./stream -nl $nl --dist=CM --numVectors=$numVectors  --messages
-./stream -nl $nl --dist=CM --numVectors=$numVectors  --timeit
+./stream -nl $nl --dist=CM --messages
+./stream -nl $nl --dist=CM --timeit
 
 echo 'no distribution (NONE)'
-./stream -nl $nl --dist=NONE --numVectors=$numVectors --timeit
+./stream -nl $nl --dist=NONE --timeit
