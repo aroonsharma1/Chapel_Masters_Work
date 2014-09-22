b2fc817142e99ed2de93078b708c9cea52be0ce9
Aroon Sharma <aroonsharma1@yahoo.com>
2014-09-22 10:57:31 -0400

    added folding vary input script

--- Josh's Benchmarks/Chapel_Benchmarks/Benchmarks and Scripts_Modified/foldingvaryinput.bash
+++ Josh's Benchmarks/Chapel_Benchmarks/Benchmarks and Scripts_Modified/foldingvaryinput.bash
@@ -8,7 +8,11 @@ echo iterations=$iterations
 #compile folding
 chpl folding.chpl --fast -o folding
 
-for n in {100..5000..100}
+for n in {100..500..100}
 	do
-		echo "$n"
+		echo "Cyclic (C) n=$n"
+		./folding -nl $nl --dist=C --n=$n --iterations=$iterations --messages --timeit
+
+		echo "Cyclic with modulo unrolling (CM) n=$n"
+		./folding -nl $nl --dist=CM --n=$n --iterations=$iterations --correct --messages --timeit
 	done
